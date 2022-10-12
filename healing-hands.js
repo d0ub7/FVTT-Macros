26fe8f1c0bae521cdf0b126b274e7778432cdbbc
deano <wickedtestament@gmail.com>
2022-10-11 20:08:24 -0400

    fix: :sparkles: update to v10

--- healing-hands.js
+++ healing-hands.js
@@ -12,22 +12,22 @@ function healersHands() {
   const r = new Roll("1d20").evaluate({ async: false });
 
   // add heal mod to the d20 roll
-  const heal_check = r.total + actor.data.data.skills.hea.mod;
+  const heal_check = r.total + actor.system.skills.hea.mod + 8;
 
   // get the target actor's HD total
-  const target_hd = canvas.tokens.get(game.user.targets.ids[0]).actor.data.data.attributes.hd.total
+  const target_hd = canvas.tokens.get(game.user.targets.ids[0]).actor.system.attributes.hd.total
 
   // heal rank progression based upon table from
   // https://aonprd.com/Skills.aspx?ItemName=Heal
   const heal_rank_multiplier =
   // 20 Ranks: When you treat deadly wounds, the target recovers hit point and ability damage as if it had rested for 3 days with long-term care.
-  actor.data.data.skills.hea.rank >= 20 ? 12 : 
+  actor.system.skills.hea.rank >= 14 ? 12 : 
   // 15 Ranks: When you treat deadly wounds, the creature recovers hit point and ability damage as if it had rested for 3 days.
-  actor.data.data.skills.hea.rank >= 15 ? 6 :
+  actor.system.skills.hea.rank >= 10 ? 6 :
   // 10 Ranks: When you treat deadly wounds, the target recovers hit points as if it had rested for a full day with long-term care.
-  actor.data.data.skills.hea.rank >= 10 ? 4 :
+  actor.system.skills.hea.rank >= 7 ? 4 :
   // 5 Ranks: When you treat deadly wounds, the target recovers hit points and ability damage as if it had rested for a full day.
-  actor.data.data.skills.hea.rank >= 5 ? 2 : 1;
+  actor.system.skills.hea.rank >= 5 ? 2 : 1;
 
   // get base heal amount
   let heal_amount = target_hd * heal_rank_multiplier;
@@ -37,14 +37,14 @@ function healersHands() {
 
   // if we surpass the Treat Wounds DC by 5 we add the Int mod of the caster
   if (heal_check >= 25) {
-    heal_amount += actor.data.data.abilities.int.mod;
-    results_title += ` + ${actor.data.data.abilities.int.mod}[Intelligence Mod]`;
+    heal_amount += actor.system.abilities.int.mod;
+    results_title += ` + ${actor.system.abilities.int.mod}[Intelligence Mod]`;
   }
 
   // if we surpass the Treat Wounds DC by 10 we add the rank in knowledge plains
   if (heal_check >= 30) {
-    heal_amount += actor.data.data.skills.kpl.rank;
-    results_title += ` + ${actor.data.data.skills.kpl.rank}[Knowledge Plains Rank]`;
+    heal_amount += actor.system.skills.kpl.rank;
+    results_title += ` + ${actor.system.skills.kpl.rank}[Knowledge Plains Rank]`;
   }
 
   // construct the output HTML
@@ -60,11 +60,11 @@ function healersHands() {
       <td colspan="4" style="text-align:center">
         <a
           class="inline-result decent-roll lil-enhanced-roll"
-          title="1d20 + ${actor.data.data.skills.hea.mod}[Heal Modifier]"
+          title="1d20 + ${actor.system.skills.hea.mod}[Heal Modifier] + 3 [Healer's Kit] + 5 [Gloves of Healing]"
           data-roll-natural="${r.total}"
         >
           <span class="lil-roll roll die d20">${r.total}</span
-          ><span class="lil-bonus">+${actor.data.data.skills.hea.mod}</span
+          ><span class="lil-bonus">+${actor.system.skills.hea.mod}</span
           ><span class="lil-arrow">⇒</span
           ><span class="lil-total">${heal_check}</span></a
         >
